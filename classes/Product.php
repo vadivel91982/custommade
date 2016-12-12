e712d6689694bfd125a40f445bf05926ad2383c5
manivannan-manureva <manivannan.g@manurevasolutions.com>
2016-12-12 19:18:51 +0530

    12122016 - 7:18PM

--- classes/Product.php
+++ classes/Product.php
@@ -1,149 +1,134 @@
 <?php
-class Product extends ProductCore{
-    public static function priceCalculation($id_shop, $id_product, $id_product_attribute, $id_country, $id_state, $zipcode, $id_currency,
-        $id_group, $quantity, $use_tax, $decimals, $only_reduc, $use_reduc, $with_ecotax, &$specific_price, $use_group_reduction,
-        $id_customer = 0, $use_customer_price = true, $id_cart = 0, $real_quantity = 0)
-    {
+
+class Product extends ProductCore {
+    /*
+     * module: custommade
+     * date: 2016-12-12 07:26:49
+     * version: 1.0.0
+     */
+
+    public static function priceCalculation($id_shop, $id_product, $id_product_attribute, $id_country, $id_state, $zipcode, $id_currency, $id_group, $quantity, $use_tax, $decimals, $only_reduc, $use_reduc, $with_ecotax, &$specific_price, $use_group_reduction, $id_customer = 0, $use_customer_price = true, $id_cart = 0, $real_quantity = 0) {
         static $address = null;
         static $context = null;
-
         if ($address === null) {
             $address = new Address();
         }
-
         if ($context == null) {
             $context = Context::getContext()->cloneContext();
         }
-
-        if ($id_shop !== null && $context->shop->id != (int)$id_shop) {
-            $context->shop = new Shop((int)$id_shop);
+        if ($id_shop !== null && $context->shop->id != (int) $id_shop) {
+            $context->shop = new Shop((int) $id_shop);
         }
-
         if (!$use_customer_price) {
             $id_customer = 0;
         }
-
         if ($id_product_attribute === null) {
             $id_product_attribute = Product::getDefaultAttribute($id_product);
         }
-
-        $cache_id = (int)$id_product.'-'.(int)$id_shop.'-'.(int)$id_currency.'-'.(int)$id_country.'-'.$id_state.'-'.$zipcode.'-'.(int)$id_group.
-            '-'.(int)$quantity.'-'.(int)$id_product_attribute.
-            '-'.(int)$with_ecotax.'-'.(int)$id_customer.'-'.(int)$use_group_reduction.'-'.(int)$id_cart.'-'.(int)$real_quantity.
-            '-'.($only_reduc?'1':'0').'-'.($use_reduc?'1':'0').'-'.($use_tax?'1':'0').'-'.(int)$decimals;
-
-        // reference parameter is filled before any returns
+        $cache_id = (int) $id_product . '-' . (int) $id_shop . '-' . (int) $id_currency . '-' . (int) $id_country . '-' . $id_state . '-' . $zipcode . '-' . (int) $id_group .
+                '-' . (int) $quantity . '-' . (int) $id_product_attribute .
+                '-' . (int) $with_ecotax . '-' . (int) $id_customer . '-' . (int) $use_group_reduction . '-' . (int) $id_cart . '-' . (int) $real_quantity .
+                '-' . ($only_reduc ? '1' : '0') . '-' . ($use_reduc ? '1' : '0') . '-' . ($use_tax ? '1' : '0') . '-' . (int) $decimals;
         $specific_price = SpecificPrice::getSpecificPrice(
-            (int)$id_product,
-            $id_shop,
-            $id_currency,
-            $id_country,
-            $id_group,
-            $quantity,
-            $id_product_attribute,
-            $id_customer,
-            $id_cart,
-            $real_quantity
+                        (int) $id_product, $id_shop, $id_currency, $id_country, $id_group, $quantity, $id_product_attribute, $id_customer, $id_cart, $real_quantity
         );
-
         if (isset(self::$_prices[$cache_id])) {
-            /* Affect reference before returning cache */
+
             if (isset($specific_price['price']) && $specific_price['price'] > 0) {
                 $specific_price['price'] = self::$_prices[$cache_id];
             }
             return self::$_prices[$cache_id];
         }
-
-        // fetch price & attribute price
-        $cache_id_2 = $id_product.'-'.$id_shop;
+        $cache_id_2 = $id_product . '-' . $id_shop;
         if (!isset(self::$_pricesLevel2[$cache_id_2])) {
             $sql = new DbQuery();
             $sql->select('product_shop.`price`, product_shop.`ecotax`');
             $sql->from('product', 'p');
-            $sql->innerJoin('product_shop', 'product_shop', '(product_shop.id_product=p.id_product AND product_shop.id_shop = '.(int)$id_shop.')');
-            $sql->where('p.`id_product` = '.(int)$id_product);
+            $sql->innerJoin('product_shop', 'product_shop', '(product_shop.id_product=p.id_product AND product_shop.id_shop = ' . (int) $id_shop . ')');
+            $sql->where('p.`id_product` = ' . (int) $id_product);
             if (Combination::isFeatureActive()) {
                 $sql->select('IFNULL(product_attribute_shop.id_product_attribute,0) id_product_attribute, product_attribute_shop.`price` AS attribute_price, product_attribute_shop.default_on');
-                $sql->leftJoin('product_attribute_shop', 'product_attribute_shop', '(product_attribute_shop.id_product = p.id_product AND product_attribute_shop.id_shop = '.(int)$id_shop.')');
+                $sql->leftJoin('product_attribute_shop', 'product_attribute_shop', '(product_attribute_shop.id_product = p.id_product AND product_attribute_shop.id_shop = ' . (int) $id_shop . ')');
             } else {
                 $sql->select('0 as id_product_attribute');
             }
-
             $res = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
-
             if (is_array($res) && count($res)) {
+                //echo '----' . __LINE__ . '----' . __FILE__ . '<pre>' . print_r($res, true) . '</pre>';
                 foreach ($res as $row) {
+                    $customPrice = $row['price'];
+                    if ($row['id_product_attribute'] == 0) {
+                        //$cookie = new Cookie('psAdmin');
+                        //echo '----' . __LINE__ . '----' . __FILE__ . $cookie->id_employee;
+                        //echo '----' . __LINE__ . '----' . __FILE__ . $id_product;
+                        $cookie = new Cookie('crop_data');
+                        //echo '----' . __LINE__ . '----' . __FILE__ . '<pre>' . print_r($cookie->user_cart_id, true) . '</pre>';
+
+                        $cropSessionData = unserialize($cookie->product_crop_data);
+                        
+                        if(isset($cropSessionData[$id_product]) && trim($cropSessionData[$id_product]) != ''){
+                            $customJsonData = json_decode($cropSessionData[$id_product]);
+                            $customPrice = $customJsonData->customPrice;
+                        }
+                        
+                    } else {
+                        $customPrice = $row['price'];
+                    }
+
                     $array_tmp = array(
+                        'price' => $customPrice,
                         //'price' => $row['price'],
-                        'price' => '11.11',
                         'ecotax' => $row['ecotax'],
                         'attribute_price' => (isset($row['attribute_price']) ? $row['attribute_price'] : null)
                     );
-                    self::$_pricesLevel2[$cache_id_2][(int)$row['id_product_attribute']] = $array_tmp;
-
+                    //echo '----' . __LINE__ . '----' . __FILE__ . '<pre>' . print_r($array_tmp, true) . '</pre>';
+                    self::$_pricesLevel2[$cache_id_2][(int) $row['id_product_attribute']] = $array_tmp;
                     if (isset($row['default_on']) && $row['default_on'] == 1) {
                         self::$_pricesLevel2[$cache_id_2][0] = $array_tmp;
                     }
                 }
             }
         }
-
-        if (!isset(self::$_pricesLevel2[$cache_id_2][(int)$id_product_attribute])) {
+        if (!isset(self::$_pricesLevel2[$cache_id_2][(int) $id_product_attribute])) {
             return;
         }
-
-        $result = self::$_pricesLevel2[$cache_id_2][(int)$id_product_attribute];
-
+        $result = self::$_pricesLevel2[$cache_id_2][(int) $id_product_attribute];
         if (!$specific_price || $specific_price['price'] < 0) {
-            $price = (float)$result['price'];
+            $price = (float) $result['price'];
         } else {
-            $price = (float)$specific_price['price'];
+            $price = (float) $specific_price['price'];
         }
-        // convert only if the specific price is in the default currency (id_currency = 0)
         if (!$specific_price || !($specific_price['price'] >= 0 && $specific_price['id_currency'])) {
             $price = Tools::convertPrice($price, $id_currency);
             if (isset($specific_price['price']) && $specific_price['price'] >= 0) {
                 $specific_price['price'] = $price;
             }
         }
-
-        // Attribute price
         if (is_array($result) && (!$specific_price || !$specific_price['id_product_attribute'] || $specific_price['price'] < 0)) {
-            $attribute_price = Tools::convertPrice($result['attribute_price'] !== null ? (float)$result['attribute_price'] : 0, $id_currency);
-            // If you want the default combination, please use NULL value instead
+            $attribute_price = Tools::convertPrice($result['attribute_price'] !== null ? (float) $result['attribute_price'] : 0, $id_currency);
             if ($id_product_attribute !== false) {
                 $price += $attribute_price;
             }
         }
-
-        // Tax
         $address->id_country = $id_country;
         $address->id_state = $id_state;
         $address->postcode = $zipcode;
-
-        $tax_manager = TaxManagerFactory::getManager($address, Product::getIdTaxRulesGroupByIdProduct((int)$id_product, $context));
+        $tax_manager = TaxManagerFactory::getManager($address, Product::getIdTaxRulesGroupByIdProduct((int) $id_product, $context));
         $product_tax_calculator = $tax_manager->getTaxCalculator();
-
-        // Add Tax
         if ($use_tax) {
             $price = $product_tax_calculator->addTaxes($price);
         }
-
-        // Eco Tax
         if (($result['ecotax'] || isset($result['attribute_ecotax'])) && $with_ecotax) {
             $ecotax = $result['ecotax'];
             if (isset($result['attribute_ecotax']) && $result['attribute_ecotax'] > 0) {
                 $ecotax = $result['attribute_ecotax'];
             }
-
             if ($id_currency) {
                 $ecotax = Tools::convertPrice($ecotax, $id_currency);
             }
             if ($use_tax) {
-                // reinit the tax manager for ecotax handling
                 $tax_manager = TaxManagerFactory::getManager(
-                    $address,
-                    (int)Configuration::get('PS_ECOTAX_TAX_RULES_GROUP_ID')
+                                $address, (int) Configuration::get('PS_ECOTAX_TAX_RULES_GROUP_ID')
                 );
                 $ecotax_tax_calculator = $tax_manager->getTaxCalculator();
                 $price += $ecotax_tax_calculator->addTaxes($ecotax);
@@ -151,21 +136,14 @@ class Product extends ProductCore{
                 $price += $ecotax;
             }
         }
-
-        // Reduction
         $specific_price_reduction = 0;
         if (($only_reduc || $use_reduc) && $specific_price) {
             if ($specific_price['reduction_type'] == 'amount') {
                 $reduction_amount = $specific_price['reduction'];
-
                 if (!$specific_price['id_currency']) {
                     $reduction_amount = Tools::convertPrice($reduction_amount, $id_currency);
                 }
-
                 $specific_price_reduction = $reduction_amount;
-
-                // Adjust taxes if required
-
                 if (!$use_tax && $specific_price['reduction_tax']) {
                     $specific_price_reduction = $product_tax_calculator->removeTaxes($specific_price_reduction);
                 }
@@ -176,35 +154,29 @@ class Product extends ProductCore{
                 $specific_price_reduction = $price * $specific_price['reduction'];
             }
         }
-
         if ($use_reduc) {
             $price -= $specific_price_reduction;
         }
-
-        // Group reduction
         if ($use_group_reduction) {
             $reduction_from_category = GroupReduction::getValueForProduct($id_product, $id_group);
             if ($reduction_from_category !== false) {
-                $group_reduction = $price * (float)$reduction_from_category;
+                $group_reduction = $price * (float) $reduction_from_category;
             } else { // apply group reduction if there is no group reduction for this category
                 $group_reduction = (($reduc = Group::getReductionByIdGroup($id_group)) != 0) ? ($price * $reduc / 100) : 0;
             }
-
             $price -= $group_reduction;
         }
-
         if ($only_reduc) {
             return Tools::ps_round($specific_price_reduction, $decimals);
         }
-
         $price = Tools::ps_round($price, $decimals);
-
         if ($price < 0) {
             $price = 0;
         }
-
         self::$_prices[$cache_id] = $price;
         return self::$_prices[$cache_id];
     }
+
 }
+
 ?>
