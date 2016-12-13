46d1d893dcf1d750e071eb8e13d54543383b2667
manivannan-manureva <manivannan.g@manurevasolutions.com>
2016-12-13 11:49:11 +0530

    11:49 AM

--- views/js/orderconfirm.js
+++ views/js/orderconfirm.js
@@ -1,3 +1,17 @@
+/**
+* NOTICE OF LICENSE
+*
+* This source file is subject to a commercial license from 202 ecommerce
+* Use, copy, modification or distribution of this source file without written
+* license agreement from 202 ecommerce is strictly forbidden.
+*
+* @author    202 ecommerce <contact@202-ecommerce.com>
+* @copyright Copyright (c) 202 ecommerce 2017
+* @license   Commercial license
+*
+* Support <support@202-ecommerce.com>
+*/
+
 if (window.location.hostname == '192.168.3.127' || window.location.hostname == 'localhost' || window.location.hostname == 'localprojects.com') {
     var getUrl = window.location;
     var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1]+"/";
