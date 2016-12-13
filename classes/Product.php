24dbb678ac5b7696d302de41f079e7d6277508fa
manivannan-manureva <manivannan.g@manurevasolutions.com>
2016-12-13 11:52:19 +0530

    11:52 AM

--- classes/Product.php
+++ classes/Product.php
@@ -1,3 +1,5 @@
+<?php
+
 /*
  * 2007-2017 PrestaShop
  *
@@ -22,8 +24,6 @@
  *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
-
-<?php
 
 class Product extends ProductCore {
     /*
