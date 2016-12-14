b935e972e060b3879a427f6320c04c7437c72ecc
vadivel91982 <vadivel82@gmail.com>
2016-12-14 10:53:31 +0530

    10.53

--- controllers/admin/CustomMadeAdminController.php
+++ controllers/admin/CustomMadeAdminController.php
@@ -1,17 +1,17 @@
 <?php
 /**
-* NOTICE OF LICENSE
-*
-* This source file is subject to a commercial license from 202 ecommerce
-* Use, copy, modification or distribution of this source file without written
-* license agreement from 202 ecommerce is strictly forbidden.
-*
-* @author    202 ecommerce <contact@202-ecommerce.com>
-* @copyright Copyright (c) 202 ecommerce 2014
-* @license   Commercial license
-*
-* Support <support@202-ecommerce.com>
-*/
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to a commercial license from 202 ecommerce
+ * Use, copy, modification or distribution of this source file without written
+ * license agreement from 202 ecommerce is strictly forbidden.
+ *
+ * @author    202 ecommerce <contact@202-ecommerce.com>
+ * @copyright Copyright (c) 202 ecommerce 2014
+ * @license   Commercial license
+ *
+ * Support <support@202-ecommerce.com>
+ */
 
 if (!defined('_PS_VERSION_')) {
     die(header('HTTP/1.0 404 Not Found'));
